36e491c4dbd4babc5da7e27dd7c88041e834e2a0
shede333 <333wshw@163.com>
2022-01-04 23:56:51 +0800

    update version to 1.4.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ README = Path(__file__).resolve().with_name("README.md").read_text()
 
 setup(
     name='PyMobileProvision',  # 包名字
-    version='1.4.1',  # 包版本
+    version='1.4.2',  # 包版本
     author='shede333',  # 作者
     author_email='333wshw@163.com',  # 作者邮箱
     keywords='mobileprovision mobile provision MobileProvision profile profiles',
